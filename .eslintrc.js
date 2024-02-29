fb853c55c244ff77590f35808546f581e3c5527f
Rafael Henrique Gonçalves de Oliveira <rafael.h.g.oliveira@gmail.com>
2024-02-29 15:22:34 -0300

    removido algumas regras do airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,8 @@ module.exports = {
   },
   rules: {
     'react/jsx-filename-extension': 0,
+    'react/prefer-stateless-function': 0,
+    'react/no-unused-state': 0,
+    'react/state-in-constructor': 0,
   },
 };
